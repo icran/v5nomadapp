da441258eca358bdb5dbd908c0a5af7466f698b4
parkminho <minho4377@daum.net>
2018-12-19 02:36:28 +0900

    #4-12 LogIn with Username and Password part Two

--- redux/modules/user.js
+++ redux/modules/user.js
@@ -1,19 +1,40 @@
 // Imports
 
-
-import {API_URL} from "../../constants";
+import { API_URL } from "../../constants";
+import { AsyncStorage } from "react-native";
 
 // Actions
 
+const LOG_IN = "LOG_IN";
+const LOG_OUT = "LOG_OUT";
+const SET_USER = "SET_USER";
+
 // Action Creators
 
-// API Actions
+function setLogIn(token) {
+    return {
+        type: LOG_IN,
+        token
+    };
+}
 
-function login(username, password){
-    return dispatch =>{
-        fetch(`${API_URL}/rest-auth/login/`,{
+function setUser(user) {
+    return {
+        type: SET_USER,
+        user
+    };
+}
+
+function logOut() {
+    return { type: LOG_OUT };
+}
+
+// API Actions
+function login(username, password) {
+    return dispatch => {
+        fetch(`${API_URL}/rest-auth/login/`, {
             method: "POST",
-            headers:{
+            headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
@@ -21,8 +42,15 @@ function login(username, password){
                 password
             })
         })
-        .then(response => response.json())
-        .then(json => console.log(json));
+            .then(response => response.json())
+            .then(json => {
+                if (json.token) {
+                    dispatch(setLogIn(json.token));
+                }
+                if (json.user) {
+                    dispatch(setUser(json.user));
+                }
+            });
     };
 }
 
@@ -36,6 +64,12 @@ const initialState = {
 
 function reducer(state = initialState, action) {
     switch (action.type) {
+        case LOG_IN:
+            return applyLogIn(state, action);
+        case LOG_OUT:
+            return applyLogOut(state, action);
+        case SET_USER:
+            return applySetUser(state, action);
         default:
             return state;
     }
@@ -43,14 +77,39 @@ function reducer(state = initialState, action) {
 
 // Reducer Functions
 
+function applyLogIn(state, action) {
+    const { token } = action;
+    return {
+        ...state,
+        isLoggedIn: true,
+        token
+    };
+}
+
+function applyLogOut(state, action) {
+    AsyncStorage.clear();
+    return {
+        ...state,
+        isLoggedIn: false,
+        token: ""
+    };
+}
+
+function applySetUser(state, action) {
+    const { user } = action;
+    return {
+        ...state,
+        profile: user
+    };
+}
+
 // Exports
 
-const actionCreators ={
+const actionCreators = {
     login
 };
 
-
-export {actionCreators};
+export { actionCreators };
 // Default Reducer Export
 
 export default reducer;
