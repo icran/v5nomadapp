c05fe443642cc9a233f98b283817c0380141a256
parkminho <minho4377@daum.net>
2019-01-21 21:45:46 +0900

    #4-25 Making the FeedScreen

--- screens/FeedScreen/container.js
+++ screens/FeedScreen/container.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import FeedScreen from "./presenter";
 import {Image}  from "react-native";
 import NavButton from "../../components/NavButton";
+import PropTypes from "prop-types";
 
 class Container extends Component{
     // static navigationOptions = ({navigation}) =>({
@@ -21,13 +22,33 @@ class Container extends Component{
     //     )
     // })
 
-    
+    static propTypes = {
+        feed:PropTypes.array.isRequired,
+        getFeed:PropTypes.func.isRequired,
+    }
+
+    state={
+        isFetching:false
+    }
+    componentWillReceiveProps = (nextProps) => {
+        if(nextProps.feed){
+            this.setState({
+                isFetching:false
+            });
+        }
+    }
     render(){
         
         return(
-            <FeedScreen {...this.props}/>
+            <FeedScreen {...this.state} refresh={this._refresh}/>
         )
     }
+    _refresh = () => {
+        const { getFeed } = this.props;
+        this.setState({
+            isFetching: true
+        });
+        getFeed();
+    };
 }
-
 export default Container;
--- screens/FeedScreen/index.js
+++ screens/FeedScreen/index.js
@@ -1,5 +1,27 @@
 import {connect} from "react-redux";
 import Container from "./container";
+import { actionCreators as photoActions} from "../../redux/modules/phtos";
+import { actionCreators as userActions } from "../../redux/modules/user";
 
 
-export default connect()(Container);
+
+const mapStateToProps = (state, ownProps) => {
+    const { photos: { feed} } = state
+    return{
+        feed
+    }
+}
+
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    getFeed: () => {
+      dispatch(photoActions.getFeed());
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Container);
